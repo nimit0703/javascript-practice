8f5a4d7e7ff9ffa54878665d19d007f17deccec4
nimit0703 <nimit.190410107143@gmail.com>
2023-09-21 11:38:09 +0530

    modified code using lodash

--- javascript/simple-projects/lodash implemntation/script.js
+++ javascript/simple-projects/lodash implemntation/script.js
@@ -1,7 +1,7 @@
 //const
 //objects
 const fruites = {
-  apples: { price: 52, amount: 61 },
+  apple: { price: 52, amount: 61 },
   mango: { price: 120, amount: 73 },
   orange: { price: 20, amount: 3 },
   banana: { price: 12, amount: 352 },
@@ -18,18 +18,17 @@ setInterval(() => {
   document.getElementById(
     "time"
   ).innerHTML = `<h1>${date.getHours()}: </h1><h2>${date.getMinutes()}: </h2><h3>${date.getSeconds()}</h3>`;
-  // console.log(date);
 }, 1000);
 
 //windos method
 window.onload = () => {
-  let str = "Welcome ,To Fruit Shop".split("");
+  let str = _.split("Welcome ,To Fruit Shop", "");
 
   const interval = setInterval(() => {
-    let msg = (document.getElementById("welcome").innerHTML += str[0]);
-    str = str.slice(1);
+    document.getElementById("welcome").innerHTML += str[0];
+    str = _.slice(str, 1);
 
-    if (!str.length) {
+    if (!_.size(str)) {
       clearInterval(interval);
     }
   }, 600);
@@ -39,13 +38,13 @@ window.onload = () => {
 };
 //functions
 function addFruitToCart(pices, price) {
+  console.log("enter");
   var amount = pices * price;
   var shipping = amount >= 250 ? 0 : 99;
   var totalBill = amount + shipping;
   document.getElementById("amount").innerHTML = amount;
   document.getElementById("shipping").innerHTML = shipping;
   document.getElementById("total-bill").innerHTML = totalBill;
-
 
   /**Promise
    * that everything is added to cart Successfully
@@ -56,137 +55,52 @@ function addFruitToCart(pices, price) {
   });
 }
 
-function addMangoes() {
-  //let
-  let weHave = fruites.mango.amount;
-  let cusWant = Number(document.getElementById("cust-mango-amount").value);
+function addFruit(fruitName) {
+  let fruit = _.get(fruites, fruitName, 1);
+  let weHave = fruit.amount;
+  console.log(weHave, fruit);
+  let cusWant = Number(document.getElementById(`cust-${fruitName}-amount`).value);
   if (cusWant === 0) {
-    document.getElementById("mango-error").classList.add("text-danger");
-    setTimeout(() => {
-      document.getElementById("mango-error").classList.remove("text-danger");
+    document.getElementById(`${fruitName}-error`).classList.add("text-danger");
+    _.delay(() => {
+      document.getElementById(`${fruitName}-error`).classList.remove("text-danger");
+
     }, 1000);
   } else if (cusWant > weHave) {
     document.getElementById(
-      "mango-error"
+      `${fruitName}-error`
     ).innerHTML = `We have only ${weHave} Pc`;
-    document.getElementById("mango-error").classList.add("text-danger");
+    document.getElementById(`${fruitName}-error`).classList.add("text-danger");
   } else {
     if (confirm("Press a button!\nEither OK or Cancel.")) {
-      let price = fruites.mango.price;
-      /**
-       * 
-       * here is 
-       * our
-       * promise call
-       * 
-       */
-      addFruitToCart(cusWant, price).then((msg) => {
-        console.log("msg"+msg);
+      let price = fruit.price;
+      addFruitToCart(cusWant, price).then((msg)=>{
         let itemLeft = weHave - cusWant;
-        fruites.mango.amount = itemLeft;
-        document.getElementById("mango-ammount").innerHTML = fruites.mango.amount;
-        document.getElementById("cust-mango-amount").value = 0;
+        console.log("before:",fruit,fruites);
+        _.set(fruit,'amount',itemLeft);
+        _.set(fruites,fruitName,fruit);
+        console.log("After:",fruit,fruites);
+        document.getElementById(`${fruitName}-amount`).innerHTML =fruit.amount;
+        document.getElementById(`cust-${fruitName}-amount`).value = 0;
       });
     }
   }
-
-}
-function addOranges() {
-  let weHave = fruites.orange.amount;
-  let cusWant = Number(document.getElementById("cust-orange-amount").value);
-  if (cusWant === 0) {
-    document.getElementById("orange-error").classList.add("text-danger");
-    setTimeout(() => {
-      document.getElementById("orange-error").classList.remove("text-danger");
-    }, 1000);
-  } else if (cusWant > weHave) {
-    document.getElementById(
-      "orange-error"
-    ).innerHTML = `We have only ${weHave} Pc`;
-    document.getElementById("orange-error").classList.add("text-danger");
-  } else {
-    if (confirm("Press a button!\nEither OK or Cancel.")) {
-      let price = fruites.orange.price;
-      addFruitToCart(cusWant, price);
-      let itemLeft = weHave - cusWant;
-      fruites.orange.amount = itemLeft;
-      document.getElementById("orange-ammount").innerHTML =
-        fruites.orange.amount;
-      document.getElementById("cust-orange-amount").value = 0;
-    }
-  }
-}
-function addBananas() {
-  let weHave = fruites.banana.amount;
-  let cusWant = Number(document.getElementById("cust-banana-amount").value);
-  if (cusWant === 0) {
-    document.getElementById("banana-error").classList.add("text-danger");
-    setTimeout(() => {
-      document.getElementById("banana-error").classList.remove("text-danger");
-    }, 1000);
-  } else if (cusWant > weHave) {
-    document.getElementById(
-      "banana-error"
-    ).innerHTML = `We have only ${weHave} Pc`;
-    document.getElementById("banana-error").classList.add("text-danger");
-  } else {
-    if (confirm("Press a button!\nEither OK or Cancel.")) {
-      let price = fruites.banana.price;
-      addFruitToCart(cusWant, price);
-      let itemLeft = weHave - cusWant;
-      fruites.banana.amount = itemLeft;
-      document.getElementById("banana-ammount").innerHTML =
-        fruites.banana.amount;
-      document.getElementById("cust-banana-amount").value = 0;
-    }
-  }
 }
 
-function addApples() {
-  let weHave = fruites.apples.amount;
-  let cusWant = Number(document.getElementById("cust-apple-amount").value);
-  if (cusWant === 0) {
-    document.getElementById("apple-error").classList.add("text-danger");
-    setTimeout(() => {
-      document.getElementById("apple-error").classList.remove("text-danger");
-    }, 1000);
-  } else if (cusWant > weHave) {
-    document.getElementById(
-      "apple-error"
-    ).innerHTML = `We have only ${weHave} Pc`;
-    document.getElementById("apple-error").classList.add("text-danger");
-  } else {
-    if (confirm("Press a button!\nEither OK or Cancel.")) {
-      let price = fruites.apples.price;
-      addFruitToCart(cusWant, price);
-      let itemLeft = weHave - cusWant;
-      fruites.apples.amount = itemLeft;
-      document.getElementById("apple-ammount").innerHTML =
-        fruites.apples.amount;
-      document.getElementById("cust-apple-amount").value = 0;
-    }
-  }
-}
 //function
 function setAmmountAndPrices() {
   //operations
   //events
-  document.getElementById("apple-ammount").innerHTML = fruites.apples.amount;
-  document.getElementById("apple-price").innerHTML = fruites.apples.price;
-
-  document.getElementById("mango-ammount").innerHTML = fruites.mango.amount;
-  document.getElementById("mango-price").innerHTML = fruites.mango.price;
-
-  document.getElementById("orange-ammount").innerHTML = fruites.orange.amount;
-  document.getElementById("orange-price").innerHTML = fruites.orange.price;
-
-  document.getElementById("banana-ammount").innerHTML = fruites.banana.amount;
-  document.getElementById("banana-price").innerHTML = fruites.banana.price;
+  _.forEach(fruites, (fruit, name) => {
+    // console.log(`${name}-price`, `${name}-amount`);
+    document.getElementById(`${name}-price`).innerHTML = fruit.price;
+    document.getElementById(`${name}-amount`).innerHTML = fruit.amount;
+  });
 
   var amount = 0;
   var totalBill = 0;
   var shipping = 0;
-  
+
   document.getElementById("amount").innerHTML = amount;
   document.getElementById("shipping").innerHTML = shipping;
   document.getElementById("total-bill").innerHTML = totalBill;
