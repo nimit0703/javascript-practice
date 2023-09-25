07c08e9a844cffdba840eb28752bc6be2e206175
nimit0703 <nimit.190410107143@gmail.com>
2023-09-25 16:29:33 +0530

    learn about classes , module

--- typescript/public/sandbox.js
+++ typescript/public/sandbox.js
@@ -1,10 +1,12 @@
-"use strict";
-let area = (radious) => {
-    return Math.PI * radious * radious;
-};
-let arr = ["", null, 1];
-arr.push("abc");
-arr.push(1);
-arr.push(null);
-// arr.push(true)
-console.log(arr, arr);
+import { Invoice } from './classes/Invoice.js';
+let inv1 = new Invoice('bob', 'painting ', 300);
+console.log(inv1.format());
+const forms = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+forms.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- typescript/src/sandbox.ts
+++ typescript/src/sandbox.ts
@@ -1,13 +1,32 @@
-let area = (radious:number)=>{
-    return Math.PI*radious*radious;
-};
+import { Invoice} from './classes/Invoice.js';
 
 
-let arr = ["",null,1];
-arr.push("abc");
-arr.push(1);
-arr.push(null);
-// arr.push(true)
+
+let inv1 = new Invoice('bob','painting ',300);
+console.log(inv1.format());
 
 
-console.log(arr,arr);
+
+
+
+
+
+
+const forms = document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLSelectElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+
+forms.addEventListener('submit',(e:Event)=>{
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber,
+    )
+})
