db07801ee7dbb0e97132d6eb9b5a98efb9f1e246
nimit0703 <nimit.190410107143@gmail.com>
2023-09-27 11:09:18 +0530

    minor changes

--- javascript/web-projects/loginAndSignIn/script.js
+++ javascript/web-projects/loginAndSignIn/script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    document.getElementById('signinForm').style.display ='none';
+    document.getElementById('loginForm').style.display ='none';
 }
 
 function loginClick(){
@@ -10,7 +10,26 @@ function loginClick(){
     
 }
 function signinClick(){
-    document.getElementById('form-title').innerText= 'SignIn';
+    document.getElementById('form-title').innerText= 'Sign In';
     document.getElementById('signinForm').style.display= 'block';
     document.getElementById('loginForm').style.display= 'none';
 }
+
+
+const users = [];
+
+class User{
+    constructor(name, number, email, hPassword){
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.hPassword = hPassword;
+    }
+}
+
+const signinForm = document.getElementById('signinForm'); 
+console.log(signinForm);
+
+signinForm.addEventListener("submit",function(){
+    console.log("signinClick");
+})
